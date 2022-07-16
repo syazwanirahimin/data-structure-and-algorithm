6b00c27cc442e0880ac0e6ae120f61eaef183484
Syazwani Rahimin <srahimin@outlook.com>
2022-07-17 01:05:43 +0800

    add java exercise coding

---
+++ src/FindSmallestIntervalOfKSortedLists.java
@@ -0,0 +1,12 @@
+/** Given K sorted lists of integers, find the smallest interval (inclusive) that contains at least one element
+ * from each of the k list.
+ * If there are multiple intervals of the same size, return the one that strts at the smallest number
+ *
+ * A[] = {3, 10, 15, 24}
+ * B[] = [0, 1, 2, 20}
+ * C[] = [1, 18, 21, 30}
+ *
+ * output: smallest range is: 2 from 1 to 3
+ */
+public class FindSmallestIntervalOfKSortedLists {
+}
